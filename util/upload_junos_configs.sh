10472e05406f9050a2c2ceadd4a8362cf5e86af6
Nasato Goto <goto@mfeed.ad.jp>
2019-03-22 16:22:58 +0900

    Add rs gobgpd.conf and fix something

--- util/upload_junos_configs.sh
+++ util/upload_junos_configs.sh
@@ -5,12 +5,12 @@ ssh_config=$script_dir/../tmp/ssh_config
 
 [ -f $ssh_config ] || vagrant ssh-config > $ssh_config
 
-for host in r3; do
+for host in r2 r3; do
   patch_file=$script_dir/../configs/${host}.patch
   scp -F $ssh_config $patch_file ${host}:${host}.patch
 done
 
-for host in r3; do
+for host in r2 r3; do
   ssh -F $ssh_config $host "cat << EOF | cli
 configure
 load patch ${host}.patch
