33013dab7e972233a42e11860fd07ea70a74c774
Shu Sugimoto <shu@su.gimo.to>
2019-03-23 20:34:57 +0900

    Update util scripts

--- util/save_configs.sh
+++ util/save_configs.sh
@@ -7,6 +7,6 @@ ssh_config=$root_dir/tmp/ssh_config
 [ -f $ssh_config ] || vagrant ssh-config > $ssh_config
 
 for host in r1 r2 r3; do
-  ssh -F $ssh_config -l root $host cli show configuration > $root_dir/configs/${host}.config
+  ssh -F $ssh_config -l root $host cli show configuration | grep -v '^## Last commit:' > $root_dir/configs/${host}.config
   ssh -F $ssh_config -l root $host cli show configuration \\\| compare initial_config > $root_dir/configs/${host}.patch
 done
